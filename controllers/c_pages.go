90dbe1b2db6fb51c7c471fa7a7c19791fb208591
Azuma Fan <azuma@theplant.jp>
2015-09-11 16:22:08 +0800

    hide attr

--- controllers/c_pages.go
+++ controllers/c_pages.go
@@ -27,6 +27,9 @@ func ViewPageHandler(ctx *gin.Context) {
 		codes := strings.Split(strings.TrimSpace(page.ProductCodes), ",")
 		for _, code := range codes {
 			p := hybris.GetProduct(code)
+			if p == nil {
+				continue
+			}
 			ps := *p
 			db.DB.Where(db.Product{Code: p.Code}).Assign(ps).FirstOrCreate(&ps)
 		}
@@ -34,7 +37,10 @@ func ViewPageHandler(ctx *gin.Context) {
 	var products []*db.Product
 	for _, p := range page.Products {
 		p := hybris.GetProduct(p.Code)
-		p.Price = hybris.GetPrice(p.Europe1Prices.PriceRow[1].Uri)
+		if p.Europe1Prices != nil && len(p.Europe1Prices.PriceRow) == 2 {
+			p.Price = hybris.GetPrice(p.Europe1Prices.PriceRow[1].Uri)
+		}
+
 		// log.Printf("product %+v", p)
 		// log.Printf("Picture %+v", p.Picture)
 		// log.Printf("Picture %+v", p.Europe1Prices.PriceRow[1].Uri)
